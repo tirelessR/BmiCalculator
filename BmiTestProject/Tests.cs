68417f13f952074450ce15965998abb96287200b
Ivan Repin <repinivan05@mail.ru>
2026-09-10 11:13:49 +0300

    11:13

---
+++ BmiTestProject/Tests.cs
@@ -0,0 +1,15 @@
+using System;
+using NUnit.Framework;
+
+namespace BmiTestProject
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
