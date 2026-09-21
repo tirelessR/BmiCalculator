d8009b3425214b88c88c279f91fba382faa1a0ff
tirelessR <ipacucka@gmail.com>
2026-09-21 11:23:15 +0300

    tests refactor

--- BmiTestProject/Tests.cs
+++ BmiTestProject/Tests.cs
@@ -8,14 +8,16 @@ namespace BmiTestProject
     [TestFixture]
     public class Tests
     {
-        [Test]
-        public void Happy1()
-        {
-            double weight = 80, height = 1.7;
-            
-            double calculated = Math.Round(BmiCalculator.GetBmi(weight, height), 2);
 
-            Assert.That(calculated, Is.EqualTo(27.68));
+        [TestCase(80, 1.7)]
+        [TestCase(60, 1.8)]
+        public void HappyPath(double weight, double height)
+        {
+            double calculated = Math.Round(BmiCalculator.GetBmi(weight, height), 2);
+            double expected = Math.Round(weight / Math.Pow(height, 2), 2);
+
+            Assert.That(calculated, Is.EqualTo(expected).Within(0.01));
         }
+
     }
 }
